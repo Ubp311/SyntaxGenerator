2c96fd3496ebbd8ea2fd3ec84967080d9eaecc06
Ubp311 <Ununennium302@gmail.com>
2023-06-01 00:09:45 +0900

    v1.3
    Add partial random indices generation.

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include	<fstream>
 #include	<random>
 #include	<string>
+#include	<cmath>
 
 
 using namespace std;
@@ -36,25 +37,52 @@ string	itos(int val)
 	return	resultStr;
 };
 
-int flag(const string& str, bool& isSameLength, bool& isUnsigned)
+string	uitous(unsigned int	val)
 {
-	if(str == "-sl" && !isSameLength)
+	string	resultStr;
+
+	unsigned int	tempVal = val;
+	unsigned int	mul = 1U;
+
+	if(val == 0U)
+		return "0";
+	tempVal /= 10U;
+	while(tempVal != 0U)
 	{
-		isSameLength = true;
-		return	1;
+		tempVal /= 10U;
+		mul *= 10U;
 	}
-	else if(str == "-u" && !isUnsigned)
+	while(mul != 0U)
 	{
-		isUnsigned = true;
-		return	1;
+		resultStr += val / mul + '0';
+		val -= val / mul * mul;
+		mul /= 10U;
 	}
-	else
-		return	0;
+
+	return	resultStr;
+};
+
+int flag(const string& str, bool flags[])
+{
+	const string	flagStrs[4] = {"-v", "-sl", "-u", "-r"};
+
+	for(int i = 0; i < 4; i++)
+	{
+		if(!str.compare(flagStrs[i]) && !flags[i])
+		{
+			flags[i] = true;
+			return	1;
+		}
+		else
+			return	0;
+	}
+
+	return	0;
 };
 
 bool	flagError(const string& str)
 {
-	if(!str.compare("-sl") || !str.compare("-u"))
+	if(!str.compare("-v") || !str.compare("-sl") || !str.compare("-u") || !str.compare("-r"))
 		return	false;
 	else
 	{
@@ -90,7 +118,7 @@ bool	charError(const char& c)
 	}
 };
 
-const string	bigIntDivisionStr = "def bigIntDivision(val1, val2):\n"
+/*const string	bigIntDivisionStr = "def bigIntDivision(val1, val2):\n"
 	"\tremainder = 0\n"
 	"\ttempRemainder = 0\n"
     "\tvalStr1Length = len(str(val1))\n"
@@ -130,12 +158,28 @@ const string	bigIntDivisionStr = "def bigIntDivision(val1, val2):\n"
     "\t\t\t\t\ttempQEst = int(tempQEst / 2)\n"
 	"\t\t\tresultStr += str(qEst)\n"
 	"\t\t\treturn  int(resultStr)\n"
-	"\treturn	int(val1 / val2)\n\n";
+	"\treturn	int(val1 / val2)\n\n";*/
 const string	bigIntMultiplyStr = "def bigIntMultiply(val1, val2):\n"
 	"\tqEst = bigIntDivision(val1, val2)\n"
 	"\tif val1 > val2 * qEst and val1 < val2 * (qEst + 1) or val1 == val2 * qEst:\n"
 	"\t\treturn	True\n"
 	"\treturn	False\n\n";
+const string	encodeStr = "def encode(val):\n"
+	"\tvals = []\n"
+	"\twhile val >= 2 ** 32:\n"
+	"\t\tvals.append(val % 2 ** 32)\n"
+	"\t\tval //= 2 ** 32\n"
+	"\tvals.append(val)\n"
+	"\treturn	vals\n\n";
+const string	randomIndicesDecodeStr = "def randomIndicesDecode(vals, s, e):\n"
+	"\tresult = 0\n"
+	"\tif s < len(vals) and e >= len(vals):\n"
+	"\t\te = len(vals) - 1\n"
+	"\telif s >= len(vals) and e >= len(vals):\n"
+	"\t\treturn 0\n"
+	"\tfor i in range(s, e + 1):\n"
+	"\t\tresult += (2 ** 32) ** i * vals[i]\n"
+	"\treturn	result\n\n";
 
 int main(int argc, char* argv[])
 {
@@ -143,8 +187,7 @@ int main(int argc, char* argv[])
 	int	operand1MinDigitNum = 8, operand1MaxDigitNum = 100;
 	int operand2MinDigitNum = 8, operand2MaxDigitNum = 100;
 	char	opChar = '+';
-	bool	isSameLength = false;
-	bool	isUnsigned = false;
+	bool	flags[4] = {false, false, false, false}; // array by value, sameLength, unsigned, random indices
 	char	flagChar = 'n';
 
 	if (argc == 1)
@@ -158,22 +201,34 @@ int main(int argc, char* argv[])
 			cout << "Error : Unknown operator." << endl;
 			return	1;
 		}
-		cout << "Make the first and second value the same length? (y/n) : ";
+		cout << "Array by array or Array by variable? (a/v) : ";
 		cin >> flagChar;
-		isSameLength = (flagChar == 'y') ? true : false;
+		flags[0] = (flagChar == 'a') ? true : false;
+		if(flags[0])
+		{
+			cout << "Make the first and second value the same length? (y/n) : ";
+			cin >> flagChar;
+			flags[1] = (flagChar == 'y') ? true : false;
+		}
 		cout << "Signed value? (y/n) : ";
 		cin >> flagChar;
-		isUnsigned = (flagChar == 'y') ? false : true;
-		if(!isSameLength)
+		flags[2] = (flagChar == 'y') ? true : false;
+		cout << "Random range? (y/n) : ";
+		cin >> flagChar;
+		flags[3] = (flagChar == 'y') ? true : false;
+		if(flags[0])
 		{
 			cout << "Input the first minimum digit : ";
 			cin >> operand1MinDigitNum;
 			cout << "Input the first maximum digit : ";
 			cin >> operand1MaxDigitNum;
-			cout << "Input the second minimum digit : ";
-			cin >> operand2MinDigitNum;
-			cout << "Input the second maximum digit : ";
-			cin >> operand2MaxDigitNum;
+			if (!flags[1])
+			{
+				cout << "Input the second minimum digit : ";
+				cin >> operand2MinDigitNum;
+				cout << "Input the second maximum digit : ";
+				cin >> operand2MaxDigitNum;
+			}
 		}
 		else
 		{
@@ -195,43 +250,43 @@ int main(int argc, char* argv[])
 		
 		if(flagError(argv[i]))
 			return	1;
-		i += flag(argv[i], isSameLength, isUnsigned);
+		i += flag(argv[i], flags);
 		if(flagError(argv[i]))
 			return	1;
-		i += flag(argv[i], isSameLength, isUnsigned);
+		i += flag(argv[i], flags);
 		syntaxNum = atoi(argv[i++]);
 		if(flagError(argv[i]) || charError(argv[i][0]))
 			return	1;
-		i += flag(argv[i], isSameLength, isUnsigned);
+		i += flag(argv[i], flags);
 		if(flagError(argv[i]))
 			return	1;
-		i += flag(argv[i], isSameLength, isUnsigned);
+		i += flag(argv[i], flags);
 		opChar = argv[i++][0];
 		if(flagError(argv[i]))
 			return	1;
-		i += flag(argv[i], isSameLength, isUnsigned);
+		i += flag(argv[i], flags);
 		if(flagError(argv[i]))
 			return	1;
-		i += flag(argv[i], isSameLength, isUnsigned);
+		i += flag(argv[i], flags);
 		operand1MinDigitNum = atoi(argv[i++]);
-		i += flag(argv[i], isSameLength, isUnsigned);
+		i += flag(argv[i], flags);
 		if(flagError(argv[i]))
 			return	1;
-		i += flag(argv[i], isSameLength, isUnsigned);
+		i += flag(argv[i], flags);
 		if(flagError(argv[i]))
 			return	1;
 		operand1MaxDigitNum = atoi(argv[i++]);
-		i += flag(argv[i], isSameLength, isUnsigned);
+		i += flag(argv[i], flags);
 		if(flagError(argv[i]))
 			return	1;
-		i += flag(argv[i], isSameLength, isUnsigned);
+		i += flag(argv[i], flags);
 		if(flagError(argv[i]))
 			return	1;
 		operand2MinDigitNum = atoi(argv[i++]);
-		i += flag(argv[i], isSameLength, isUnsigned);
+		i += flag(argv[i], flags);
 		if(flagError(argv[i]))
 			return	1;
-		i += flag(argv[i], isSameLength, isUnsigned);
+		i += flag(argv[i], flags);
 		if(flagError(argv[i]))
 			return	1;
 		operand2MaxDigitNum = atoi(argv[i++]);
@@ -239,13 +294,13 @@ int main(int argc, char* argv[])
 		{
 			if(flagError(argv[i]))
 				return	1;
-			i += flag(argv[i], isSameLength, isUnsigned);
+			i += flag(argv[i], flags);
 		}
 		if(i < argc)
 		{
 			if(flagError(argv[i]))
 				return	1;
-			i += flag(argv[i], isSameLength, isUnsigned);
+			i += flag(argv[i], flags);
 		}
 	}
 
@@ -257,10 +312,11 @@ int main(int argc, char* argv[])
 
 	int	index = 1;
 
-	random_device	rd1, rd2, rd3;
+	random_device	rd1, rd2, rd3, rd4;
 	mt19937	gen1(rd1()), gen2(rd2()), gen3(rd3());
 	uniform_int_distribution<int>	disRange1(operand1MinDigitNum, operand1MaxDigitNum), disRange2(operand2MinDigitNum, operand2MaxDigitNum);
 	uniform_int_distribution<int> disVal(0, 9), disSign(0, 1);
+	uniform_int_distribution<unsigned int>	disIndex(0U, -1);
 
 	fileAddr += itos(index++);
 
@@ -287,63 +343,117 @@ int main(int argc, char* argv[])
 	oFileTxt.open(fileAddr + ".txt", ios_base::out);
 	if (!oFilePy.fail() && !oFileTxt.fail())
 	{
-		oFilePy.write(bigIntDivisionStr.c_str(), bigIntDivisionStr.size());
-		oFilePy.write(bigIntMultiplyStr.c_str(), bigIntMultiplyStr.size());
+		oFilePy.write(encodeStr.c_str(), encodeStr.size());
+		oFilePy.write(randomIndicesDecodeStr.c_str(), randomIndicesDecodeStr.size());
+		if(!flags[0])
+			oFileTxt.write("v\n", 2);
 		for (int i = 0; i < syntaxNum; i++)
 		{
 			string	pyStr = "print(";
 			string	syntaxStr;
-			bool	isNegative = false;
-			if(opChar == '/')
-				pyStr += "bigIntDivision(";
-			if(!isUnsigned)
-				isNegative = disSign(gen3);
+			string	tempStr;
 			int	maxJ = disRange1(gen1);
 			int	tempVal = disVal(gen2);
+			bool	isNegative = false;
+
+			if(flags[2])
+			{
+				isNegative = disSign(gen3);
+				pyStr += "abs(";
+			}
 			if (isNegative)
 			{
 				pyStr += '-';
 				syntaxStr += '-';
 			}
+			if(flags[3])
+			{
+				pyStr += "randomIndicesDecode(encode(";
+				syntaxStr += '(';
+			}
 			while (tempVal == 0)
 				tempVal = disVal(gen2);
 			for (int j = 0; j < maxJ; j++)
 			{
-				pyStr += tempVal + '0';
-				syntaxStr += tempVal + '0';
+				tempStr += tempVal + '0';
 				tempVal = disVal(gen2);
 			}
-			if(opChar == '/')
-				pyStr += ",";
-			else
+			pyStr += tempStr;
+			syntaxStr += tempStr;
+			if(flags[3])
 			{
-				pyStr += ' '; // * / % + -
-				pyStr += opChar;
+				pyStr += ')';
+				size_t maxIndex = (unsigned long long)(log2(10.0) * tempStr.size() + 2) >> 5;
+
+				uniform_int_distribution<unsigned int>	disIndex(0, maxIndex);
+				size_t	s, e;
+
+				s = disIndex(rd4);
+				e = disIndex(rd4);
+				if(s > e)
+					swap(s, e);
+				pyStr += ", " + uitous(s) + ", " + uitous(e) + ')';
+				syntaxStr += ", " + uitous(s) + ", " + uitous(e) + ')';
 			}
 			pyStr += ' ';
+			if (opChar == '/')
+				pyStr += opChar;
+			pyStr += opChar;
+			pyStr += ' ';
 			syntaxStr += ' ';
-			syntaxStr += opChar;
+			syntaxStr += opChar; // * / % + -
 			syntaxStr += ' ';
-			if(!isSameLength)
+			if (flags[2])
+				isNegative = disSign(gen3);
+			if (flags[0])
 			{
-				maxJ = disRange2(gen1);
-				if(!isUnsigned)
-					isNegative = disSign(gen3);
+				
+				if (!flags[1])
+					maxJ = disRange2(gen1);
+				if (isNegative)
+				{
+					pyStr += '-';
+					syntaxStr += '-';
+				}
+				if(flags[3])
+				{
+					pyStr += "randomIndicesDecode(encode(";
+					syntaxStr += '(';
+				}
+				while (tempVal == 0)
+					tempVal = disVal(gen2);
+				for (int j = 0; j < maxJ; j++)
+				{
+					pyStr += tempVal + '0';
+					syntaxStr += tempVal + '0';
+					tempVal = disVal(gen2);
+				}
+				if(flags[3])
+				{
+					pyStr += ')';
+
+					size_t maxIndex = (unsigned long long)(log2(10.0) * tempStr.size() + 2) >> 5;
+
+					uniform_int_distribution<unsigned int> disIndex(0, maxIndex);
+					size_t s, e;
+
+					s = disIndex(rd4);
+					e = disIndex(rd4);
+					if (s > e)
+						swap(s, e);
+					pyStr += ", " + uitous(s) + ", " + uitous(e) + ')';
+					syntaxStr += ", " + uitous(s) + ", " + uitous(e) + ')';
+				}
 			}
-			if (isNegative)
+			else
 			{
-				pyStr += '-';
-				syntaxStr += '-';
+				string	tempStr;
+
+				tempStr = uitous(disIndex(rd4));
+				pyStr += tempStr;
+				syntaxStr += tempStr;
 			}
-			while (tempVal == 0)
-				tempVal = disVal(gen2);
-			for (int j = 0; j < maxJ; j++)
-			{
-				pyStr += tempVal + '0';
-				syntaxStr += tempVal + '0';
-				tempVal = disVal(gen2);
-			}
-			if(opChar == '/')
+			if(flags[2])
 				pyStr += ')';
 			pyStr += ")\n";
 			if(i < syntaxNum - 1)
